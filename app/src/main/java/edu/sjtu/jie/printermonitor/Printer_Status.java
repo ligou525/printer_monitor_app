7a73c169f38f3f76884ed578d22e077aa97800ad
gouli <gouli@baidu.com>
2018-07-05 01:21:47 +0800

    socket

--- app/src/main/java/edu/sjtu/jie/printermonitor/Printer_Status.java
+++ app/src/main/java/edu/sjtu/jie/printermonitor/Printer_Status.java
@@ -1,11 +1,14 @@
 package edu.sjtu.jie.printermonitor;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class Printer_Status extends AppCompatActivity {
 
@@ -13,6 +16,14 @@ public class Printer_Status extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_printer__status);
+//        ImageButton imageButton=findViewById(R.id.image_iat_set);
+//        ImageView imageView=findViewById(R.id.print_status_img);
+////        Resources resources = getResources();
+////        image.setImageDrawable(resources.getDrawable(R.drawable.myfirstimage));
+//        Resources resources=getResources();
+//        imageButton.setImageDrawable(resources.getDrawable(R.drawable.main_setting_btn_np));
+//        imageView.setImageDrawable(resources.getDrawable(R.drawable.printer_example));
+//
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
